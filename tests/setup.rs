0ac31a271f799e30c770473b3bc6d8c55d70e46d
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-07-31 21:16:53 +0200

    prepared tls setup

--- tests/setup.rs
+++ tests/setup.rs
@@ -1,6 +1,7 @@
 use axum::Extension;
 use axum_test::TestServer;
 use forumfactor::app::app;
+use serde::{Deserialize, Serialize};
 use velvet_web::prelude::{sqlite, JWT};
 
 pub async fn setup() -> TestServer {
@@ -13,4 +14,32 @@ pub async fn setup() -> TestServer {
 async fn test_setup() {
     let server = setup().await;
     server.get("/").await.assert_status_ok();
+}
+
+#[tokio::test]
+async fn test_post_topic() {
+    #[derive(Serialize)]
+    struct Form {
+        title: String,
+        text: String,
+    }
+    #[derive(Deserialize)]
+    struct Response {
+        id: String,
+        title: Option<String>,
+        text: Option<String>
+    }
+    let form = Form {
+        title: "title".into(),
+        text: "post".into(),
+    };
+
+    let server = setup().await;
+    let post = server.post("/topic").form(&form).await;
+    post.assert_status_ok();
+    let post_id = post.json::<Response>().id;
+
+    let post = server.get(format!("/topic/{}", post_id).as_str()).await;
+    post.assert_status_ok();
+    let response = post.json::<Response>();
 }
