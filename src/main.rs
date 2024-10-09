6ab6a5ea8b20c1c16d782661d0d880818aeb24c5
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-10-09 14:42:08 +0200

    upgrade and use login

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,20 @@ async fn main() {
     #[folder = "static"]
     struct S;
 
+    #[derive(Deserialize)]
+    #[allow(dead_code)]
+    struct Claims {
+        username: String,
+    }
+
     dotenvy::dotenv().ok();
     let db = sqlite().await;
     sqlx::migrate!().run(&db).await.unwrap();
 
     App::new()
-        .router(app::app())
+        .router(app::app().authorized_cookie_claims("/login", |_: Claims| Ok(AuthResult::OK)))
+        .login_flow(&db)
+        .await
         .inject(db)
         .statics::<S>()
         .start()
